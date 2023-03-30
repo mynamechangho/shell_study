9c0131ba39f28c035ff196335dd6ede2770badbd
ChangHo_Yoon <128906852+mynamechangho@users.noreply.github.com>
2023-03-30 21:23:32 +0900

    Update homework.sh

--- homework.sh
+++ homework.sh
@@ -1,11 +1,11 @@
 o "숫자를 입력하시오 >>"
 read num
         if [ $num -lt 0 ]
-		    then
-			                echo "음수:" "$num"
-					        elif [ $num -gt 0 ]
-							    then
-								                echo "양수:" "$num"
-										        else
-												        echo "The number is 0:" "$num"
-													        fi
+    then
+    		echo "음수:" "$num"
+        elif [ $num -gt 0 ]
+    then
+                echo "양수:" "$num"
+        else
+        echo "The number is 0:" "$num"
+	fi
